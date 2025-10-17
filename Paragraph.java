f3c13f3aba909c6812b08e13d58a2196bf569a37
darius2303 <dariuscristoi@gmail.com>
2025-10-17 13:33:10 +0300

    Lab2

--- Paragraph.java
+++ Paragraph.java
@@ -1,12 +1,21 @@
 public class Paragraph implements Element {
     private String text;
+    private AlignStrategy alignStrategy;
 
     public Paragraph(String text) {
         this.text = text;
     }
 
+    public void setAlignStrategy(AlignStrategy alignStrategy) {
+        this.alignStrategy = alignStrategy;
+    }
+
     @Override
     public void print() {
-        System.out.println("Paragraph: " + text);
+        if (alignStrategy != null) {
+            alignStrategy.render(text);
+        } else {
+            System.out.println("Paragraph: " + text);
+        }
     }
 }
